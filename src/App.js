32d451df0b28c4c559f74104acc85533666ce9be
Karabo-L <karabolesapo2@gmail.com>
2024-10-27 14:29:11 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ import "./App.css";
 
 export default function App() {
   return (
-    <div className="App">
-      Weather App
-      <footer>
-        This project was coded by Karabo Lesapo and it is open sourced on{" "}
-        <a
-          href="https://github.com/Karabo-L/final_react_weather_app"
-          target="blank"
-        >
-          GitHub
-        </a>
-      </footer>
+    <div className="App ">
+      <div classname="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Karabo Lesapo and it is open sourced on{" "}
+          <a
+            href="https://github.com/Karabo-L/final_react_weather_app"
+            target="blank"
+          >
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
