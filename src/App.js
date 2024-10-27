da2d8843b1921a3deeeed00d4020d5179a32908a
Karabo-L <karabolesapo2@gmail.com>
2024-10-27 13:51:39 +0200

    added the github rep url

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
 
-function App() {
-  return <div className="App">Weather App</div>;
+export default function App() {
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by Karabo Lesapo and it is open sourced on{" "}
+        <a
+          href="https://github.com/Karabo-L/final_react_weather_app"
+          target="blank"
+        >
+          GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
-
-export default App;
