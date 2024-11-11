c36e9b48da6f28f713ab4646174b73e9ef727a94
Karabo-L <karabolesapo2@gmail.com>
2024-11-11 11:44:42 +0200

    added target blanks to the footer links

--- src/App.js
+++ src/App.js
@@ -32,9 +32,16 @@ function App() {
       </main>
       <footer>
         This project was coded by Karabo Lesapo and is open-sourced on{" "}
-        <a href="https://github.com/Karabo-L/final_react_weather_app">GitHub</a>{" "}
+        <a
+          href="https://github.com/Karabo-L/final_react_weather_app"
+          target="blank"
+        >
+          GitHub
+        </a>{" "}
         and hosted on{" "}
-        <a href="https://shecodes-react-meteo-app.netlify.app/">Netlify</a>
+        <a href="https://shecodes-react-meteo-app.netlify.app/" target="blank">
+          Netlify
+        </a>
       </footer>
     </div>
   );
